3909d3e7393d7f7d9f01de54dc0b2075273c1819
Wykthor <wykthorgcv@gmail.com>
2024-04-28 15:47:52 -0300

    changes

--- soundboard/src/app/page.tsx
+++ soundboard/src/app/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 import { PauseCircleOutlined, PlayCircleOutlined, RedoOutlined, SoundOutlined } from "@ant-design/icons";
-import { Badge, Button, Checkbox, Col, Form, FormProps, Image, Input, List, message, Modal, Progress, Row, Slider, Switch, Tabs, TabsProps } from "antd";
-import React, { useEffect, useState } from "react";
+import autoAnimate from "@formkit/auto-animate";
+import { Badge, Button, Checkbox, Col, Form, FormProps, Image, Input, List, message, Modal, Progress, Row, Slider, Switch, Tabs, TabsProps, Tooltip } from "antd";
+import React, { useEffect, useRef, useState } from "react";
 import { useAudioPlayer } from "react-use-audio-player";
 // @ts-ignore
 
@@ -65,51 +66,94 @@ function Fighter(props: any) {
     border: "1px solid grey",
     flex: 1
   }
+  const currentRef = useRef(null);
   const [value, setValue] = useState("0")
+  const [shield, setShield] = useState(0)
   const [stats, setStats] = useState({
     name: props.item.name,
     stoppingPower: props.item.stoppingPower,
+    stoppingPowerHead: props.item.stoppingPower,
     stoppingPowerMax: props.item.stoppingPower,
+    stoppingPowerHeadMax: props.item.stoppingPower,
     currentHealth: props.item.currentHealth,
     maxHealth: props.item.maxHealth
   })
-
+  useEffect(()=>{
+    if(props.currentTurn == props.index && currentRef.current) {
+      currentRef.current.scrollIntoView({behavior: "smooth"})
+    }
+  },[props.currentTurn])
   function heal () {
     setStats({...stats,
       currentHealth: Math.min(stats.currentHealth+Number(value),stats.maxHealth)})
     }
-    
+  
+  function addShield () {
+    setShield(Number(value))
+  }
   function damage () {
-    setStats({...stats,
-      currentHealth: Math.max(stats.currentHealth-Number(value),0)})
+    var val = Number(value)
+    if(shield > val) {
+      setShield(Math.max(shield-Number(value),0))
+    } else if(shield==val) {
+      setShield(0)
     }
+    else {
+      val = val-shield
+      setShield(0)
+      setStats({...stats,
+        currentHealth: Math.max(stats.currentHealth-(val),0)})
+    }}
 
   function ablate () {
     setStats({...stats,
       stoppingPower: Math.max(stats.stoppingPower-1, 0)})
   }
 
-  function resetShield () {
+  function resetStoppingPower () {
     setStats({...stats,
       stoppingPower: stats.stoppingPowerMax})
   }
+  function ablateHead () {
+    setStats({...stats,
+      stoppingPowerHead: Math.max(stats.stoppingPowerHead-1, 0)})
+  }
+
+  function resetStoppingPowerHead () {
+    setStats({...stats,
+      stoppingPowerHead: stats.stoppingPowerHeadMax})
+  }
   return (
     <Badge.Ribbon color="red" text={props.currentTurn == props.index ? "Sua vez!" : ""}>
-      <List.Item>
+      <List.Item ref={currentRef}>
         <Row style={style}>
           <Col span={24}>
             {stats.name}
           </Col>
           <Col span={12}>
             <span>
-              HP: <Progress 
+              HP: <span style={{fontWeight: "bold"}}>
+                {stats.currentHealth/stats.maxHealth<=0.5 
+                ? (stats.currentHealth < 1 ? 
+                    <Tooltip title=" -4 to all Actions. -6 to MOVE(Min. 1) Death save at start of each turn (Roll 1d10<BODY = success) Critical Injury when damaged. Death Save Penalty +! on hit.">Mortally Wounded!</Tooltip> 
+                : "Seriously Wounded! -2 to all Actions.") : null}</span>
+              <Progress 
+                    success={{percent: (stats.currentHealth/(stats.maxHealth+shield))*100, strokeColor: (stats.currentHealth/stats.maxHealth)*100<=50 ? "red" : "green"}}
                     showInfo={false} 
-                    strokeColor={(stats.currentHealth/stats.maxHealth)*100<=25 ? "red" : "green"} 
-                    percent={(stats.currentHealth/stats.maxHealth)*100}/>
+                    strokeColor={"purple"} 
+                    percent={shield != 0 ? (stats.currentHealth+shield/stats.maxHealth+shield)*100 : 0}
+                    />
             </span>
           </Col>
           <Col span={12}>
-            SP: {stats.stoppingPower}<Progress showInfo={false} strokeColor={"blue"} percent={(stats.stoppingPower/stats.stoppingPowerMax)*100}/>
+            <Row>
+              <Col span={12}>
+                SP Head: {stats.stoppingPowerHead}<Progress showInfo={false} strokeColor={"orange"} percent={(stats.stoppingPowerHead/stats.stoppingPowerHeadMax)*100}/>
+              </Col>
+              <Col span={12}>
+                SP Body: {stats.stoppingPower}<Progress showInfo={false} strokeColor={"orange"} percent={(stats.stoppingPower/stats.stoppingPowerMax)*100}/>
+              </Col>
+            </Row>
           </Col>
           <Col span={24}>
             <Row>
@@ -120,16 +164,20 @@ function Fighter(props: any) {
                 <Button style={{width: 100}} onClick={heal}>Heal</Button>
               </Col>
               <Col span={3}>
-                <Input onChange={(event)=>setValue(event.target.value)} value={value}></Input>
+                <Button style={{width: 100}} onClick={addShield}>Shield</Button>
               </Col>
-              <Col style={{justifyContent: "center"}} span={3}>
-                <Checkbox>Critically Injured?</Checkbox>
+              <Col span={3}>
+                <Input onChange={(event)=>setValue(event.target.value)} value={value}></Input>
               </Col>
               <Col span={12} style={{textAlign: "end"}}>
                 <Row justify={"end"}>
                   <Col span={12}>
+                    <Button onClick={ablateHead}>Ablate</Button>
+                    <Button onClick={resetStoppingPowerHead}>Reset SP</Button>
+                  </Col>
+                  <Col span={12}>
                     <Button onClick={ablate}>Ablate</Button>
-                    <Button onClick={resetShield}>Reset SP</Button>
+                    <Button onClick={resetStoppingPower}>Reset SP</Button>
                   </Col>
                 </Row>
               </Col>
@@ -150,7 +198,14 @@ function InitiativeTracker(props: any) {
   }
   const [data, setData] = useState<Array<Idata>>([])
   const [toggleForm, setToggleForm] = useState(false)
+  const [toggleMook, setToggleMook] = useState(false)
+  const [toggleLieutenant, setToggleLieutenant] = useState(false)
+  const [toggleMiniBoss, setToggleMiniBoss] = useState(false)
+  const [toggleBoss, setToggleBoss] = useState(false)
+  const [togglePlayers, setTogglePlayers] = useState(false)
   const [currentTurn, setCurrentTurn] = useState(0)
+  const [addFighters, setAddFighters] = useState(false)
+
   type FieldType = {
     name: string;
     SP: string;
@@ -194,192 +249,238 @@ function InitiativeTracker(props: any) {
   const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
-  
+  function OptionSwitcher (props: any) {
+    return (
+    <Row align={"middle"} style={{margin: 10, flex: 1}}>
+      <Col span={3}>
+        <Switch checkedChildren={props.switchTitle} unCheckedChildren={props.switchTitle} onChange={props.setToggleOptions} value={props.toggleOptions}/>
+      </Col>
+      <Col span={12}>
+        <Col span={24}>
+          {props.toggleOptions ? props.children : null}
+        </Col>
+      </Col>
+    </Row>) 
+  }
   return (
-    <>
-    <Row>
-      <span>Add mook</span>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "boosterganger",
-        health: "20",
-        SP: "4"
-      })}}>
-        boosterganger
-      </Button>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "bodyguard",
-        health: "35",
-        SP: "7"
-      })}}>
-        bodyguard
-      </Button>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "road ganger",
-        health: "25",
-        SP: "4"
-      })}}>
-        road ganger
-      </Button>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "security operative",
-        health: "30",
-        SP: "7"
-      })}}>
-        security operative
-      </Button>
-    </Row>
-    <Row>
-      <span>Add lieutenant</span>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "netrunner",
-        health: "30",
-        SP: "11"
-      })}}>
-        netrunner
-      </Button>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "reclaimer chief",
-        health: "40",
-        SP: "11"
-      })}}>
-        reclaimer chief
-      </Button>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "security officer",
-        health: "40",
-        SP: "13"
-      })}}>
-        security officer
-      </Button>
-    </Row>
-    <Row>
-      <span>Add mini-boss</span>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "outrider",
-        health: "40",
-        SP: "11"
-      })}}>
-        security officer
-      </Button>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "pyro",
-        health: "40",
-        SP: "11"
-      })}}>
-        pyro
-      </Button>
-    </Row>
-    <Row>
-      <span>Add boss</span>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "cyberpsycho",
-        health: "55",
-        SP: "11"
-      })}}>
-        cyberpsycho
-      </Button>
-    </Row>
-    <Row>
-      <span>Add players</span>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "Glamour",
-        health: "35",
-        SP: "11"
-      })}}>
-        Glamour
-      </Button>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "Kiwi",
-        health: "35",
-        SP: "11"
-      })}}>
-        Kiwi
-      </Button>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "Loverruk",
-        health: "45",
-        SP: "11"
-      })}}>
-        Loverruk
-      </Button>
-      <Button onClick={
-        ()=>{addNPC({
-        name: "Paniki",
-        health: "50",
-        SP: "11"
-      })}}>
-        Paniki
-      </Button>
-    </Row>
-      <List dataSource={data} renderItem={(item, index)=>(
-        <Fighter item={item} index={index} currentTurn={currentTurn}/>
-        )}
-        footer={
+    <Row style={{flexDirection: "column"}}>
+      <Row>
+        <Col span={24}>
           <>
-          <Row align={"middle"} gutter={[16,16]}>
-            <Col><Button onClick={()=>{setData([])}}>Clear battle</Button></Col>
-            <Col>
-              <Switch onChange={setToggleForm}/>
-            </Col>
-            <Col>Add new combatant</Col>
-            <Col><Button onClick={previousTurn}>Previous Turn</Button></Col>
-            <Col><Button onClick={nextTurn}>Next Turn</Button></Col>
-          </Row>
-          {toggleForm ? 
-          <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          style={{ maxWidth: 600, border: "solid 1px grey", padding: "8px 16px", borderRadius: 20}}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item<FieldType>
-            label="Name"
-            name="name"
+            <Row align={"middle"} gutter={[16,16]}>
+              <Col><Button onClick={()=>{setData([])}}>Clear battle</Button></Col>
+              <Col>
+                <Switch onChange={setToggleForm}/>
+              </Col>
+              <Col>Add new combatant</Col>
+              <Col>
+                <Switch onChange={setAddFighters}/>
+              </Col>
+              <Col>Add preset combatant</Col>
+              <Col><Button onClick={previousTurn}>Previous Turn</Button></Col>
+              <Col><Button onClick={nextTurn}>Next Turn</Button></Col>
+            </Row>
+            {toggleForm ?
+            <Form
+            name="basic"
+            layout="vertical"
+            labelCol={{ span: 10 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600, border: "solid 1px grey", padding: "8px 16px", borderRadius: 20}}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
           >
-            <Input />
-          </Form.Item>
-      
-          <Form.Item<FieldType>
-            label="Stopping Power"
-            name="SP"
-          >
-            <Input />
-          </Form.Item>
-      
-          <Form.Item<FieldType>
-            label="Health"
-            name="health"
-          >
-            <Input />
-          </Form.Item>
-      
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-          </Form>: null
-          }
+            <Form.Item<FieldType>
+              label="Name"
+              name="name"
+            >
+              <Input />
+            </Form.Item>
+        
+            <Form.Item<FieldType>
+              label="Stopping Power"
+              name="SP"
+            >
+              <Input />
+            </Form.Item>
+        
+            <Form.Item<FieldType>
+              label="Health"
+              name="health"
+            >
+              <Input />
+            </Form.Item>
+        
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+            </Form>: null
+            }
           </>
-        }/>
-    </>
+        </Col>
+      </Row>
+      {addFighters ? <>
+      <Row>
+        <OptionSwitcher switchTitle="Add Mook" toggleOptions={toggleMook} setToggleOptions={setToggleMook}>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "boosterganger",
+            health: "20",
+            SP: "4"
+          })}}>
+            boosterganger
+          </Button>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "bodyguard",
+            health: "35",
+            SP: "7"
+          })}}>
+            bodyguard
+          </Button>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "road ganger",
+            health: "25",
+            SP: "4"
+          })}}>
+            road ganger
+          </Button>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "security operative",
+            health: "30",
+            SP: "7"
+          })}}>
+            security operative
+          </Button>
+        </OptionSwitcher>
+      </Row>
+      <Row>
+        <OptionSwitcher switchTitle="Add Lieutenant" toggleOptions={toggleLieutenant} setToggleOptions={setToggleLieutenant}>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "netrunner",
+            health: "30",
+            SP: "11"
+          })}}>
+            netrunner
+          </Button>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "reclaimer chief",
+            health: "40",
+            SP: "11"
+          })}}>
+            reclaimer chief
+          </Button>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "security officer",
+            health: "40",
+            SP: "13"
+          })}}>
+            security officer
+          </Button>
+        </OptionSwitcher>
+      </Row>
+      <Row>
+        <OptionSwitcher switchTitle="Add mini-boss" toggleOptions={toggleMiniBoss} setToggleOptions={setToggleMiniBoss}>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "outrider",
+            health: "40",
+            SP: "11"
+          })}}>
+            outrider
+          </Button>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "pyro",
+            health: "40",
+            SP: "11"
+          })}}>
+            pyro
+          </Button>
+        </OptionSwitcher>
+      </Row>
+      <Row>
+        <OptionSwitcher switchTitle="Add boss" toggleOptions={toggleBoss} setToggleOptions={setToggleBoss}>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "cyberpsycho",
+            health: "55",
+            SP: "11"
+          })}}>
+            cyberpsycho
+          </Button>
+        </OptionSwitcher>
+      </Row>
+      <Row>
+        <OptionSwitcher switchTitle="Add players" toggleOptions={togglePlayers} setToggleOptions={setTogglePlayers}>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "Glamour",
+            health: "35",
+            SP: "11"
+          })}}>
+            Glamour
+          </Button>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "Kiwi",
+            health: "35",
+            SP: "11"
+          })}}>
+            Kiwi
+          </Button>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "Loverruk",
+            health: "45",
+            SP: "11"
+          })}}>
+            Loverruk
+          </Button>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "Paniki",
+            health: "50",
+            SP: "11"
+          })}}>
+            Paniki
+          </Button>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "MSN",
+            health: "40",
+            SP: "11"
+          })}}>
+            MSN
+          </Button>
+          <Button onClick={
+            ()=>{addNPC({
+            name: "Cobra",
+            health: "40",
+            SP: "11"
+          })}}>
+            Cobra
+          </Button>
+        </OptionSwitcher>
+      </Row>
+      </> : null}
+      <Row>
+        <Col span={24} style={{overflowY: "scroll", height: 600}}>
+            <List dataSource={data} renderItem={(item, index)=>(
+              <Fighter item={item} index={index} currentTurn={currentTurn}/>
+              )}/>
+        </Col>
+      </Row>
+    </Row>
   )
 }
 
